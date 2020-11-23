93e851ca9614b1e18c2db18ca4486b3b6246855f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-23 08:53:45 +0100

    Ho modificato il codice aggiungendo una parte di codice che chiede una frase in input.

--- EsUniCode/Program.cs
+++ EsUniCode/Program.cs
@@ -13,10 +13,14 @@ namespace EsUniCode
             string unicodeString = "This Unicode string has 2 characters outside the " + 
                 "ASCII range: \n"+ 
                 "Pi (\u03A0), and sigma (\u03A3)";
+            
+            Console.Write("Inserisci una frase: ");
+            unicodeString = Console.ReadLine();
+            
             Console.WriteLine("Original string: ");
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.WriteLine(unicodeString);
-            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
