3c9ce6959b793edbb10b1cf2d5ba0d525d8550be
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-23 09:21:52 +0100

    Ho aggiunto un commento dove spiego cosa significa la riga 18.

--- EsUniCode/Program.cs
+++ EsUniCode/Program.cs
@@ -10,15 +10,16 @@ namespace EsUniCode
     {
         static void Main(string[] args)
         {
-            string unicodeString = "This Unicode string has 2 characters outside the " + 
-                "ASCII range: \n"+ 
-                "Pi (\u03A0), and sigma (\u03A3)";
-            
+
+            //string unicodeString = "This Unicode string has 2 characters outside the " + 
+            //    "ASCII range: \n"+ 
+            //    "Pi (\u03A0), and sigma (\u03A3)";
+
             Console.Write("Inserisci una frase: ");
-            unicodeString = Console.ReadLine();
-            
+            string unicodeString = Console.ReadLine();
+
             Console.WriteLine("Original string: ");
-            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;//Questo comando serve per convertire dei simboli per poi stamparli a video in unicode.
             Console.WriteLine(unicodeString);
             Console.ReadLine();
         }
